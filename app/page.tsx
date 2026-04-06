5fea096ab75aa60225ba17ad7741d911bbc9f020
yogeshk34 <khutwadyogesh34@gmail.com>
2026-04-06 22:22:06 +0530

    refactor: page.tsx to remove TodoComponent

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <div className="pointer-events-none absolute inset-x-0 top-[-140px] -z-10 mx-auto h-[360px] w-[92%] rounded-[3rem] bg-gradient-to-r from-cyan-200/50 via-emerald-200/40 to-amber-200/45 blur-3xl" />
       <div className="space-y-8">
         <HabitTracker />
-        <Todo />
+        {/* <Todo /> */}
       </div>
     </main>
   );
